8acd826e11bc4fd449d029ade9973456db2548fe
Mujeeb <mujeebmalik2502@gmail.com>
2026-01-22 23:14:29 +0530

    Initial EasySplit setup with React and Vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial"}}>
+      <h1>EasySplit</h1>
+      <p>Smart group expense splitting made simple.</p>
+    </div>
+  )
+}
+  
+
+export default App;
