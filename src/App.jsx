711716b6d9fac82fd852c4e6194501c87bef02fd
Mujeeb <mujeebmalik2502@gmail.com>
2026-01-22 23:33:35 +0530

    Setup routing and base pages for EasySplit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Groups from "./pages/Groups";
+import Profile from "./pages/Profile";
+
 function App() {
   return (
+    <BrowserRouter>
     <div style={{ padding: "20px", fontFamily: "Arial"}}>
       <h1>EasySplit</h1>
       <p>Smart group expense splitting made simple.</p>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
-  )
+    </BrowserRouter>
+  );
 }
   
 
---
+++ src/pages/Groups.jsx
@@ -0,0 +1,4 @@
+function Groups(){
+    return <h2>Your Groups</h2>;
+}
+export default Groups;
