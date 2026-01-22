b24f33895d1142a9f88f5564c9afb98e39902c1e
Mujeeb <mujeebmalik2502@gmail.com>
2026-01-23 04:11:20 +0530

    Display user profile and implement logout functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,32 @@
-function Profile(){
-    return <h2> Profile</h2>;
+import { signOut } from "firebase/auth";
+import { auth } from "../services/firebase";
+import { useAuth } from "../context/AuthContext";
+
+function Profile() {
+  const { user } = useAuth();
+
+  const handleLogout = async () => {
+    await signOut(auth);
+  };
+
+  return (
+    <div>
+      <h2>Profile</h2>
+
+      <img
+        src={user.photoURL}
+        alt="Profile"
+        style={{ width: "80px", borderRadius: "50%" }}
+      />
+
+      <p><strong>Name:</strong> {user.displayName}</p>
+      <p><strong>Email:</strong> {user.email}</p>
+
+      <button onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  );
 }
+
 export default Profile;
